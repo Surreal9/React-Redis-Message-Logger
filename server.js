6151b6d3e8482c64ba9061f27fb13fcca262876a
RecursiveLogic <qrx9823@gmail.com>
2015-06-24 14:14:39 -0400

    Connected database to React

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-var express = require('express'), app = express(),
-	path = require('path'), http = require('http').Server(app),
-	io = require('socket.io')(http);
+var express = require('express'), app = express();
+var	path = require('path'), http = require('http').Server(app);
+var io = require('socket.io')(http), redis = require('redis');
+var database = redis.createClient();
 
 app.use(express.static(path.join(__dirname, './www')));
+
+database.on('connect', function() {
+	console.log('Connected');
+});
 
 io.on('connection', function(socket) {
 	socket.on('message', function(message) {
 		console.log(message);
+		database.set('log', message);
 	});
-	socket.emit('messageLog', 'Here is your server response.');
+	// socket.emit('messageLog', 'Here is your server response.');
+	database.get('log', function(error, response) {
+		socket.emit('messageLog', response);
+	});
 });
 
 http.listen(4664, function() {
